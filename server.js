95f5af1b97f755c80e558e5dfbc81fdca2f04b50
theddyarmeinsyah <theddyarmeinsyah85@gmail.com>
2021-04-17 00:27:38 +0700

    Menambahkan router di server.js

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const app = express();
 //parse aplication / json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+//Panggil Router
+var routes = require('router');
+routes(app);
+
 app.listen(3000, () => {
     console.log(`Server started on port`);
 });
